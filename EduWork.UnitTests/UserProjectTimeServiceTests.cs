6ab27b5c99fa16d20c7f4a0be4ec7e4ea96459cd
tonileo <mdtonileo@gmail.com>
2024-07-11 15:01:49 +0200

    Unit test project initialized with 2 simple tests

---
+++ EduWork.UnitTests/UserProjectTimeServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EduWork.BusinessLayer.Services;
+
+namespace EduWork.UnitTests
+{
+    public class UserProjectTimeServiceTests
+    {
+        [Fact]
+        public void Ctor_UserProjectTimeService_Succeeds()
+        {
+            var sut = new UserProjectTimeService(null, null);
+
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public async Task Ctor_UserProjectTimeService_Fails()
+        {
+            var sut = new UserProjectTimeService(null, null);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await sut.GetProjectTimesFilter(null, null, null, null));
+        }
+    }
+}
