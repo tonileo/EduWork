30979eccec4ddc6842c38cb9554896be32c048ce
tonileo <mdtonileo@gmail.com>
2024-09-29 19:46:41 +0200

    Revert "Implemented error handling middleware on server side"
    This reverts commit bf4b18d11c0e9ba85423e99a126ac910f08aa219.

--- EduWork.WebApi/Configurations/WebAppExtensions.cs
+++ EduWork.WebApi/Configurations/WebAppExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using EduWork.DataAccessLayer;
 using EduWork.DataAccessLayer.Seed;
-using EduWork.WebApi.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
 
@@ -14,6 +13,14 @@ namespace EduWork.WebApi.Configurations
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+
+                app.UseCors(policy =>
+                {
+                    policy.WithOrigins("https://localhost:7041")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithHeaders(HeaderNames.ContentType);
+                });
             }
 
             app.UseHttpsRedirection();
@@ -25,16 +32,6 @@ namespace EduWork.WebApi.Configurations
 
             app.UseAuthentication();
             app.UseAuthorization();
-
-            app.UseMiddleware<ExceptionMiddleware>();
-
-            app.UseCors(policy =>
-            {
-                policy.WithOrigins("https://localhost:7041")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .WithHeaders(HeaderNames.ContentType);
-            });
 
             app.MapControllers();
 
